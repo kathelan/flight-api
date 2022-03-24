38c49a159daa7addb9643d3f1f88a5bf08dcb95a
Mateusz Deptulski <Mateusz.Deptulski@energia.ee>
2022-03-24 18:47:33 +0100

    Added SkyScanner Rest Client unit tests of getToken

--- src/test/java/pl/kathelan/flightapi/SkyScannerClientUnitTest.java
+++ src/test/java/pl/kathelan/flightapi/SkyScannerClientUnitTest.java
@@ -1,5 +1,44 @@
 package pl.kathelan.flightapi;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import pl.kathelan.flightapi.configuration.services.SkyScannerClient;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+
+@ExtendWith({MockitoExtension.class})
 public class SkyScannerClientUnitTest {
+
+    @Spy
+    @InjectMocks
+    private SkyScannerClient skyScannerClient;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Test
+    void shouldGetTokenWhileFetchIsValid() {
+        Mockito.when(restTemplate.getForEntity(any(), any(), anyMap())).thenReturn(new ResponseEntity<>("anyString", HttpStatus.OK));
+
+        skyScannerClient.getToken();
+
+    }
+
+    @Test
+    void shouldGetTokenWhileFetchIsInValid() {
+        Mockito.when(restTemplate.getForEntity(any(), any(), anyMap())).thenReturn(new ResponseEntity<>("anyString", HttpStatus.INTERNAL_SERVER_ERROR));
+
+        skyScannerClient.getToken();
+
+    }
 
 }
