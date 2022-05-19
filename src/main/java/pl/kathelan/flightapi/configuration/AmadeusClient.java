dc47d94e9f25c9e296078f43849f4361ddc178d8
kathelan <mateuszdeptulski@gmail.com>
2022-05-19 23:02:53 +0200

    Revert " added secrets"
    This reverts commit 22fe9ffd

--- src/main/java/pl/kathelan/flightapi/configuration/AmadeusClient.java
+++ src/main/java/pl/kathelan/flightapi/configuration/AmadeusClient.java
@@ -1,24 +1,22 @@
 package pl.kathelan.flightapi.configuration;
 
 import com.amadeus.Amadeus;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
 
-@Slf4j
 @Service
 public class AmadeusClient {
 
-    @Value("sm://AMADEUS_CLIENT_ID")
+    private final static Logger LOGGER = Logger.getLogger(AmadeusClient.class.getName());
+
+    @Value("${clientId}")
     private String clientId;
-    @Value("sm://AMADEUS_CLIENT_SECRET")
+    @Value("${clientSecret}")
     private String clientSecret;
 
-    @PostConstruct
     public Amadeus getAmadeusObj() {
-        log.info("clientId: {} clientSecret: {}",clientId ,clientSecret);
-        return Amadeus.builder(clientId, clientSecret).build();
+        return Amadeus.builder(clientId, clientSecret).setLogger(LOGGER).build();
     }
 }
