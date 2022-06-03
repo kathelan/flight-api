873427278f930578e097990ade14694774cd352e
Mateusz Deptulski <Mateusz.Deptulski@energia.ee>
2022-06-03 16:59:56 +0200

    test

--- src/main/java/pl/kathelan/flightapi/FlightApiApplication.java
+++ src/main/java/pl/kathelan/flightapi/FlightApiApplication.java
@@ -3,7 +3,6 @@ package pl.kathelan.flightapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
-
 @SpringBootApplication(exclude = {SolrAutoConfiguration.class})
 public class FlightApiApplication {
 
